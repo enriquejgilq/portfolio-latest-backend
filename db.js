f0e57c8dd90c816c6fe65f4973622d32ec676e64
enriquejgilq <enriquegq1093@hotmail.com>
2025-01-19 20:52:43 -0500

    feature(core): update name variable

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.BD_DEV);
-        console.log("Conexión a MongoDB exitosa", process.env.BD_DEV);
+        await mongoose.connect(process.env.MONGODB);
+        console.log("Conexión a MongoDB exitosa", process.env.MONGODB);
     } catch (error) {
         console.error("Error de conexión a MongoDB:", error);
         process.exit(1);
